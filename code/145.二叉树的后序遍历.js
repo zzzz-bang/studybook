441085ce2f9906677513d964692697c426dc8f70
zzzz-bang <810119973@qq.com>
2022-01-05 22:23:35 +0800

    Add files via upload

---
+++ code/145.二叉树的后序遍历.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=javascript
+ *
+ * [145] 二叉树的后序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    let res=[]
+    if(!root) return res
+    function deep(root){
+        if(!root) return null;
+        deep(root.left);
+        deep(root.right);
+        res.push(root.val)
+    }
+    deep(root)
+    return res
+};
+// @lc code=end
+
